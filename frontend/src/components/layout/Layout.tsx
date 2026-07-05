b7fc9dc06cb7488741b3d012c08feaa59cac245e
Prince Kumar Singh <kprinces888@gmail.com>
2026-07-05 11:11:05 +0530

    Remove double Container wrapper from Layout

--- frontend/src/components/layout/Layout.tsx
+++ frontend/src/components/layout/Layout.tsx
@@ -2,7 +2,6 @@ import { Outlet } from 'react-router-dom'
 import Navbar from './Navbar'
 import Sidebar from './Sidebar'
 import SkipToContent from './SkipToContent'
-import { Container } from './Container'
 
 export default function Layout() {
   return (
@@ -12,9 +11,7 @@ export default function Layout() {
       <div className="flex flex-col md:flex-row">
         <Sidebar />
         <main id="main-content" className="flex-1 px-4 py-3 sm:px-6 lg:px-8 lg:py-5" tabIndex={-1}>
-          <Container size="xl" className="py-1">
-            <Outlet />
-          </Container>
+          <Outlet />
         </main>
       </div>
     </div>
